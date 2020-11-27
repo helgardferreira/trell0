b32f45211fe96d76904c1f9e3d0d17fa7e696971
Helgard Richard Ferreira <hrfer3@gmail.com>
2020-11-27 11:15:50 +0200

    Create trell0 Card component

---
+++ trell0/src/components/Card/index.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+import { CardModel } from "../../store/model/list.interface";
+
+const CardBackground = styled.div`
+  background-color: white;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
+  padding: 10px;
+`;
+
+const Card: FunctionComponent<CardModel> = ({ name }) => {
+  return <CardBackground>{name}</CardBackground>;
+};
+
+export default Card;
