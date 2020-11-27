755e78d9b022534c430aa2706794b04d5e6d5786
Helgard Richard Ferreira <hrfer3@gmail.com>
2020-11-27 11:14:37 +0200

    Create trell0 Board component

---
+++ trell0/src/components/Board/index.tsx
@@ -0,0 +1,47 @@
+import React, { FunctionComponent, useCallback, useEffect } from "react";
+import { useDispatch, useSelector, useStore } from "react-redux";
+import styled from "styled-components";
+
+import { BoardModel } from "../../store/model/board.model";
+import { ApplicationState } from "../../store/model";
+import { loadBoard } from "../../store/reducers/board.reducer";
+import { Container } from "../Styled/Utils";
+
+const Heading = styled.h1`
+  a {
+    text-decoration: none;
+    color: black;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Board: FunctionComponent = () => {
+  const dispatch = useDispatch();
+
+  const board = useSelector<ApplicationState, BoardModel>((state) => {
+    return state.board;
+  });
+
+  const onLoadBoard = useCallback(() => {
+    dispatch(loadBoard());
+  }, [dispatch]);
+
+  useEffect(() => {
+    onLoadBoard();
+  }, [onLoadBoard]);
+
+  return (
+    <section>
+      <Container>
+        <Heading>
+          <a href={board.url}>{board.name}</a>
+        </Heading>
+      </Container>
+    </section>
+  );
+};
+
+export default Board;
